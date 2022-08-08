b55b2dbcbe21c92a0bdd77d2daa7c229fa9565d5
jakebitz <jacobbitters@gmail.com>
2022-08-07 21:34:43 -0500

    require

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,5 @@
 // Write your helper functions here!
-// require('isomorphic-fetch');
+ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
